09baf5b1ced1737fff7eba10f38d1be6ac632980
Rockou_ <82128803+Rockouunderscore@users.noreply.github.com>
2023-01-11 22:08:41 -0500

    yes

--- Game Jam/Assets/Script/Player/PlaneSwitcherFollow.cs
+++ Game Jam/Assets/Script/Player/PlaneSwitcherFollow.cs
@@ -2,7 +2,21 @@ using UnityEngine;
 
 public class PlaneSwitcherFollow : MonoBehaviour
 {
+
+    [SerializeField] private GameObject Left;
+    [SerializeField] private GameObject Right;
+    
     [SerializeField] private GameObject _playerFollowY = null;
+    [SerializeField] private float _towerSize = 5;
+    [SerializeField] private float _offsetOutwards = 1.2f;
+
+    private void OnEnable()
+    {
+        float offset = _towerSize / 2 + _offsetOutwards;
+        Left.transform.localPosition = GameManager.Inst.ConvertVector(new Vector3(-offset, 0, 0));
+        Right.transform.localPosition = GameManager.Inst.ConvertVector(new Vector3(offset, 0, 0));
+    }
+
     private void FixedUpdate()
     {
         if (_playerFollowY == null) return;
