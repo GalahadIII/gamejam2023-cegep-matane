409e5def108fb044802beecc32c12936b13cfb4c
2001085 <hcalvet70@gmail.com>
2023-01-12 16:36:26 -0500

    Vire a gauche et a droite

--- Game Jam/Assets/Script/Player/MovementController.cs
+++ Game Jam/Assets/Script/Player/MovementController.cs
@@ -21,7 +21,7 @@ public class MovementController : MonoBehaviour
 
     public bool DisabledControls;
 
-    public Vector2 Speed => new Vector2(_rb.velocity.x, _rb.velocity.z);
+    public Vector2 Speed => new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
     public bool Falling => _rb.velocity.y < 0;
     public bool Jumping => _rb.velocity.y > 0;
     public bool FacingRight => _facingRight;
